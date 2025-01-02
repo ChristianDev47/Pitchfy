bf68993483bc776687a8ae064af0b5ac9d615561
Christian Delgado <christiands.dev@gmail.com>
2025-01-02 11:24:21 -0400

    update

--- Frontend/src/components/ui/ButtonHero.tsx
+++ Frontend/src/components/ui/ButtonHero.tsx
@@ -11,7 +11,7 @@ function ButtonReact() {
 
   return (
     <a
-      className="ml-2 py-1.5 px-5.5 md:px-6 font-semibold shadow-none text-[15px] w-auto btn btn-primary"
+      className="ml-2 py-2 px-5.5 md:px-6 font-semibold shadow-none text-[15px] w-auto btn btn-primary"
       href={user && user._id !== '' ? 'https://pitchfy.zapier.app' : '/login'}
     >
       <span>Start Your Story Now</span>
