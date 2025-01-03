577e5fef05198e37b08b30fcc74283e8a33341c4
Christian Delgado <christiands.dev@gmail.com>
2025-01-02 20:12:03 -0400

    Update

--- Frontend/src/components/ui/ButtonComponent.tsx
+++ Frontend/src/components/ui/ButtonComponent.tsx
@@ -6,9 +6,9 @@ function ButtonReact() {
   return (
     <a
       className="ml-2 py-1.5 px-5 md:px-6 font-semibold shadow-none text-[15px] w-auto btn-primary"
-      href={user && user.plan !== '' ? 'https://pitchfy.zapier.app' : user._id  !== '' ? `/plan/started` : '/login'}
+      href={user && user.plan ? 'https://pitchfy.zapier.app' : user._id  !== '' ? `/plan/started` : '/login'}
     >
-      <span>{user && user._id !== '' && user.plan !== '' ? 'Chat' : user._id !== '' ? 'Get a plan' : 'Login'}</span>
+      <span>{user && user._id !== '' && user.plan  ? 'Chat' : user._id !== '' ? 'Get a plan' : 'Login'}</span>
     </a>
   );
 }
--- Frontend/src/components/ui/ButtonHero.tsx
+++ Frontend/src/components/ui/ButtonHero.tsx
@@ -8,7 +8,7 @@ function ButtonReact() {
   return (
     <a
       className="ml-2 py-2 px-5.5 md:px-6 font-semibold shadow-none text-[15px] w-auto btn btn-primary"
-      href={user && user.plan !== '' ? 'https://pitchfy.zapier.app' : user._id  !== '' ? `/plan/started` : '/login'}
+      href={user && user.plan ? 'https://pitchfy.zapier.app' : user._id  !== '' ? `/plan/started` : '/login'}
     >
       <span>Start Your Story Now</span>
     </a>
