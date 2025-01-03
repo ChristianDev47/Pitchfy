ce4484fb7f27a1f96572777290fba3cf84ff0a8f
Christian Delgado <christiands.dev@gmail.com>
2025-01-03 09:00:19 -0400

    Update

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -22,13 +22,19 @@ export default {
       },
 
       animation: {
-        fade: 'fadeInUp 1s both',
+        fade: 'fadeInUp 2s both',
+        heroFade: 'fadeInUp 2s both',
+        headerFade: 'headerFadeIn 2s 1s both',
       },
 
       keyframes: {
         fadeInUp: {
           '0%': { opacity: 0, transform: 'translateY(2rem)' },
           '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        headerFadeIn: {
+          '0%': { opacity: 0, transform: 'translateY(-2rem)' }, 
+          '100%': { opacity: 1, transform: 'translateY(0)' },  
         },
       },
     },
